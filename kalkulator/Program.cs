e4f03073425efa2c524242399b51c2d920dafb2a
s9722 <s9722@pjwstk.edu.pl>
2018-11-20 09:37:51 +0100

    prosty kalkulator

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,6 +10,39 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
+            int liczbaPierwsza, liczbaDruga;
+
+            Console.WriteLine("PROSTY KALKULATOR \n");
+            Console.Write("Podaj pierwszą liczbę: ");
+            liczbaPierwsza=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj drugą liczbę: ");
+            liczbaDruga = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Wybierz działanie (+, -, *, /): ");
+
+            string dzialanie;
+
+            dzialanie = Console.ReadLine();
+
+            switch (dzialanie)
+            {
+                case "+":
+                    Console.WriteLine($"wynik dodawania to: {liczbaPierwsza + liczbaDruga}");
+                    break;
+                case "-":
+                    Console.WriteLine($"wynik odejmowania to: {liczbaPierwsza - liczbaDruga}");
+                    break;
+                case "*":
+                    Console.WriteLine($"wynik mnożenia to: {liczbaPierwsza * liczbaDruga}");
+                    break;
+                case "/":
+                    Console.WriteLine($"wynik dzielenia to: {liczbaPierwsza / (double)liczbaDruga}");
+                    break;
+                                        
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
